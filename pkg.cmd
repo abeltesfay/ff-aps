6a16c46e90e36323a8149f2194e4a632e00a7ace
qazui <creativedebt@gmail.com>
2022-03-04 05:28:21 -0500

    Update pkg.cmd and pkg.sh with better notes and alternatives.

--- pkg.cmd
+++ pkg.cmd
@@ -1,12 +1,12 @@
 @REM Need to zip using DEFLATE
 
 @echo[
-@echo STARTING: Creating new compressed file ff-aps.zip....
+@echo STARTING: Creating new compressed extension file....
 
 @del /Q output\ff-aps-ext.zip
 7z a -tzip output\ff-aps-ext.zip fancy_searchbox.js icons\ manifest.json
 
 @echo[
-@echo SUCCESS: File has been compressed into a zip called ff-aps.zip that can be loaded under Firefox add-ons!
+@echo SUCCESS: File has been compressed into a zip called ff-aps-ext.zip that can be loaded under Firefox add-ons!
 @echo[
 @REM @pause
--- pkg.sh
+++ pkg.sh
@@ -1,11 +1,14 @@
-# TODD
+# TODO Notes: Must be DEFLATE zip algorithm; can't be TAR-ed.
+
+echo
+echo STARTING: Creating new compressed extension file....
+
+# TODO This hasn't been tested; may need p7zip-full package
+7z a -tzip output/ff-aps-ext.zip fancy_searchbox.js icons/ manifest.json
+
+# Alternative: May be able to use zip command: https://linuxize.com/post/how-to-zip-files-and-directories-in-linux/
+# zip -q output/ff-aps-ext.zip fancy_searchbox.js icons/ manifest.json
 
 # echo
-# echo STARTING: Creating new compressed file ff-aps.zip....
-
-# TODO This hasn't been tested
-# zip -r ff-aps.zip .
-
-# echo
-# echo SUCCESS: File has been compressed into a zip called ff-aps.zip that can be loaded under Firefox add-ons!
+# echo SUCCESS: File has been compressed into a zip called ff-aps-ext.zip that can be loaded under Firefox add-ons!
 # echo
