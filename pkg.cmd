b938bbf24c72d042c59d710a2f2c2b1b81945b0f
qazui <creativedebt@gmail.com>
2022-03-07 08:33:24 -0500

    Version 1.1.0 commit with zip

--- pkg.cmd
+++ pkg.cmd
@@ -3,10 +3,10 @@
 @echo[
 @echo STARTING: Creating new compressed extension file....
 
-@del /Q output\ff-aps-ext-v1.0.zip
-7z a -tzip output\ff-aps-ext-v1.0.zip fancy_searchbox.js icons\ manifest.json
+@del /Q output\ff-aps-ext-v1.1.0.zip
+7z a -tzip output\ff-aps-ext-v1.1.0.zip fancy_searchbox.js icons\ manifest.json
 
 @echo[
-@echo SUCCESS: File has been compressed into a zip called ff-aps-ext.zip that can be loaded under Firefox add-ons!
+@echo SUCCESS: File has been compressed into a zip called ff-aps-ext-v1.1.0.zip that can be loaded under Firefox add-ons!
 @echo[
 @REM @pause
